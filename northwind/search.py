e2d56fb0a05025a653246341f251afa66023ee62
baekka1 <baekka@bc.edu>
2025-02-18 18:10:47 -0500

    move search results to its own page; fix styling

--- northwind/search.py
+++ northwind/search.py
@@ -13,25 +13,45 @@ bp = Blueprint('search', __name__)
 def search():
     if request.method == 'POST':
         item = request.form['search']
-        db = get_db()
         
-        products = db.execute(
+        return redirect(url_for('search.display_search', item=item))
+    else:
+        return render_template('index.html')
+
+def get_product(item):
+    db = get_db()
+    product = db.execute(
             'SELECT ProductName, UnitPrice'
             ' FROM Product'
             ' WHERE ProductName = ? COLLATE NOCASE', (item,)
         ).fetchall()
 
-        product_type = db.execute(
+    product_type = db.execute(
             'SElECT ProductName, UnitPrice'
             ' FROM Product, Category'
             ' WHERE CategoryName = ? COLLATE NOCASE'
             ' AND Category.id = Product.CategoryId ', (item,)
         ).fetchall()
-        if not products and not product_type:
-            return render_template('search/search-error.html')
-        if not product_type:
-            return render_template('search/search-display.html', products=products)
-        return render_template('search/search-display.html', products=product_type)
-    else:
-        return render_template('index.html')
+
+    if not product and not product_type:
+        return None
+    if not product_type:
+        return product
+    return product_type
+
+@bp.route('/search/', methods=('GET', 'POST'))
+def display_search():
+    if request.method == 'POST':
+        item = request.form['search']
+        if item:
+            return redirect(url_for('search.display_search', item=item))
+        # want to reroute to a product page if clicked on product name
+        # want to add to cart if clicked 'add to cart'
+        
     
+    # logic to display correct page
+    item = request.args.get('item', '')  # Get the query parameter
+    product = get_product(item)
+    if product is None:
+        return render_template('search/search-error.html')
+    return render_template('search/search-display.html', products=product)
